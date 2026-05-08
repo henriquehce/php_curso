77523b0a5ad30bf05c9bd6ccc1cabdf9ca3002cc
Henrique Cipriani <henriquecipriani@edu.univali.br>
2026-05-08 16:03:05 -0300

    a

--- projeto_pagina_de_cinema_web/screen-match.php
+++ projeto_pagina_de_cinema_web/screen-match.php
@@ -1,7 +1,6 @@
 <?php
 
-require "funcoes.php";
-
+require __DIR__ . "/funcoes.php";
 
 echo "Bem-vindo(a) ao screen match!\n";
 
@@ -22,7 +21,7 @@ $planoPrime = true;
 $incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
 
 echo "Nome do filme: " . $nomeFilme . "\n";
-echo "Nota do filme: " . number_format($notaFilme, 2) . "\n";
+echo "Nota do filme: $notaFilme\n";
 echo "Ano de lançamento: $anoLancamento\n";
 
 exibeMensagemLancamento($anoLancamento);
@@ -33,5 +32,30 @@ $genero = match ($nomeFilme) {
     "Se beber não case" => "comédia",
     default => "gênero desconhecido",
 };
+
 echo "O gênero do filme é: $genero\n";
 
+$filme = [
+    "nome" => "Thor: Ragnarok",
+    "ano" => 2021,
+    "nota" => 7.8,
+    "genero" => "super-herói",
+];
+
+
+echo $filme["ano"];
+
+var_dump($notas);
+sort($notas);
+var_dump($notas);
+$menorNota = min($notas);
+var_dump($menorNota);
+
+var_dump($filme['nome']);
+$posicaoDoisPontos = strpos($filme['nome'], ':');
+var_dump($posicaoDoisPontos);
+
+var_dump(substr($filme['nome'], 0, $posicaoDoisPontos));
+
+$filmeComoStringJson = json_encode($filme);
+file_put_contents(__DIR__ . '/filme.json', $filmeComoStringJson);
